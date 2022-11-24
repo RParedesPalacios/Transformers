afb0ed1cadbd3d23c7bfe883274975837aa68258
RParedesPalacios <rparedes@dsic.upv.es>
2022-11-24 16:17:21 +0100

    datasets

--- ViT/getdata.py
+++ ViT/getdata.py
@@ -1,6 +1,14 @@
 import torch
 import torchvision
 import torchvision.transforms as transforms
+from randomaug import RandAugment
+
+
+## CIFAR10: 32x32 10 classes
+## CIFAR100: 32x32 100 classes
+## FOOD101: 512x512(max) 101 classes
+## FLOWERS102: small 102 classes
+## FER2013: 48x48 7 classes grayscale
 
 def getData(name, bs, transform_train, transform_test):
     if name=="CIFAR10":
@@ -13,6 +21,7 @@ def getData(name, bs, transform_train, transform_test):
         testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=8)
 
         num_classes = 10
+        
     elif name=="CIFAR100":
         print("Running over CIFAR100")
         # Download dataset and define data loader
@@ -24,6 +33,50 @@ def getData(name, bs, transform_train, transform_test):
 
         num_classes = 100
 
+    elif name=="FOOD101":
+        print("Running over FOOD101")
+        # Download dataset and define data loader
+        trainset = torchvision.datasets.Food101(root='./data', split='train', download=True, transform=transform_train)
+        trainloader = torch.utils.data.DataLoader(trainset, batch_size=bs, shuffle=True, num_workers=8)
+
+        testset = torchvision.datasets.Food101(root='./data', split='test', download=True, transform=transform_test)
+        testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=8)
+
+        num_classes = 101
+
+    elif name=="FLOWERS102":
+        print("Running over FLOWERS102")
+        # Download dataset and define data loader
+        trainset = torchvision.datasets.Flowers102(root='./data', split='train', download=True, transform=transform_train)
+        trainloader = torch.utils.data.DataLoader(trainset, batch_size=bs, shuffle=True, num_workers=8)
+
+        testset = torchvision.datasets.Flowers102(root='./data', split='test', download=True, transform=transform_test)
+        testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=8)
+
+        num_classes = 102
 
     return trainset, trainloader, testset, testloader, num_classes
+
+
+## DATA AUGMENTATION
+def getAugmentation(img_size,rand_aug):
+    transform_train = transforms.Compose([
+        transforms.Resize((img_size,img_size)),
+        transforms.RandomCrop(img_size, padding=img_size//8),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),])
+
+    transform_test = transforms.Compose([
+        transforms.Resize((img_size,img_size)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),])
+
+    # RandAugment
+    if rand_aug:
+        N = 2
+        M = 14
+        transform_train.transforms.insert(0, RandAugment(N, M))
+
+    return transform_train,transform_test
 
--- ViT/traininig.py
+++ ViT/traininig.py
@@ -9,25 +9,26 @@ import os
 # TorchVision and data augmentation
 import torchvision
 import torchvision.transforms as transforms
-from randomaug import RandAugment
+
 
 # Models
 from ViT import ViT
 
 # Data
 from getdata import getData
+from getdata import getAugmentation
 
 
 # Some important parameters
 bs = 128 # batch size
-img_size = 32 #image size (square)
+img_size = 256 #image size (square)
 epochs = 200 # total training epochs
 rand_aug = True # use random augmentation
 img_channels=3 # number of image channels
-patch_size= 4 # patch size (square)
+patch_size= 16 # patch size (square)
 d_model=384 # dimensionality transformer representation
-N=7 # Number of transformers blocks
-heads=12 # Number of transformer block heads 
+N=6 # Number of transformers blocks
+heads=8 # Number of transformer block heads 
 load_check = False # to load a checkpoint
 
 
@@ -35,27 +36,9 @@ load_check = False # to load a checkpoint
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 # Data augmentation
-transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.Resize(img_size),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
+transform_train,transform_test=getAugmentation(img_size,rand_aug)
 
-transform_test = transforms.Compose([
-    transforms.Resize(img_size),
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
-
-# RandAugment
-if rand_aug:
-    N = 2
-    M = 14
-    transform_train.transforms.insert(0, RandAugment(N, M))
-
-trainset,trainloader,testset,testloader,num_classes=getData("CIFAR100", bs, transform_train, transform_test)
+trainset,trainloader,testset,testloader,num_classes=getData("FOOD101", bs, transform_train, transform_test)
 
 ## Model
 net=ViT(img_size, img_channels, patch_size, d_model, N, heads, num_classes)
